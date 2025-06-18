98156c3c015e00e98030e19968d5b5fedcdf6556
Drew Camron <dcamron@ucar.edu>
2025-06-18 12:43:45 -0600

    Remove binder link generator

--- site/_extensions/gallery_generator.py
+++ site/_extensions/gallery_generator.py
@@ -2,27 +2,10 @@ import itertools, json, yaml, pathlib, subprocess, requests
 from truncatehtml import truncate
 import re
 
-
-def _grab_binder_link(repo):
-    config_url = (
-        f"https://raw.githubusercontent.com/ProjectPythia/{repo}/main/_config.yml"
-    )
-    config = requests.get(config_url).content
-    config_dict = yaml.safe_load(config)
-    root = config_dict["sphinx"]["config"]["html_theme_options"]["launch_buttons"][
-        "binderhub_url"
-    ]
-    end = f"/v2/gh/ProjectPythia/{repo}.git/main"
-    url = root + end
-    return root, url
-
-
 def _generate_status_badge_html(repo, github_url):
-    binder_root, binder_link = _grab_binder_link(repo)
     github_id = _grab_github_id(repo)
     return f"""
     <a class="reference external" href="{github_url}/actions/workflows/nightly-build.yaml"><img alt="nightly-build" src="{github_url}/actions/workflows/nightly-build.yaml/badge.svg" /></a>
-    <a class="reference external" href="{binder_link}"><img alt="Binder" src="{binder_root}/badge_logo.svg" /></a>
     <a class="reference external" href="https://zenodo.org/badge/latestdoi/{github_id}"><img alt="DOI" src="https://zenodo.org/badge/{github_id}.svg" /></a>
     """
 
